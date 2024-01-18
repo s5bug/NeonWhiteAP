6c9285f24886cae2ed2a7353bcd8d4764481d5a4
Aly Cerruti <aprime2003@gmail.com>
2024-01-17 22:41:00 -0800

    Initial commit

---
+++ NeonWhitePlugin/NeonWhitePlugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace NeonWhitePlugin;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInProcess("Neon White.exe")]
+public class NeonWhitePlugin : BaseUnityPlugin {
+    public static ManualLogSource Log;
+    public static Harmony Harmony = new(PluginInfo.PLUGIN_GUID);
+    
+    private void Awake() {
+        Log = this.Logger;
+        try {
+            Harmony.PatchAll();
+        } catch (Exception e) {
+            Log.LogError(e);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+﻿### ⚠ THIS IS A TEMPLATE REPOSITORY! Do not fork, instead use the "Use this template" button!
+
+# NeonWhitePlugin
+
+Create a file `NeonWhitePlugin/NeonWhitePlugin.csproj.user` defining the path to your Neon White folder. An example
+with Windows Steam on the C drive is shown below:
+
+```msbuild
+<Project>
+    <PropertyGroup>
+        <NeonWhitePath>C:/Program Files (x86)/Steam/steamapps/common/Neon White</NeonWhitePath>
+    </PropertyGroup>
+</Project>
+```
